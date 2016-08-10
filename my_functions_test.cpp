b4fbaddacb6c34363adf6ce1bd4d702c23c19477
richelbilderbeek <richel@richelbilderbeek.nl>
2016-08-10 21:25:59 +0200

    Try to keep -Weffc++ in

--- my_functions_test.cpp
+++ my_functions_test.cpp
@@ -1,8 +1,6 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Weffc++"
 #include <boost/test/unit_test.hpp>
-#pragma GCC diagnostic pop
-
 #include "my_functions.h"
 
 BOOST_AUTO_TEST_CASE(add_works)
@@ -12,3 +10,5 @@ BOOST_AUTO_TEST_CASE(add_works)
   BOOST_CHECK(add(1, 3) == 4);
   BOOST_CHECK(add(1, 4) == 5);
 }
+
+#pragma GCC diagnostic pop
